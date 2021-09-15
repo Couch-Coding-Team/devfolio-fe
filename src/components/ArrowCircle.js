57875e877ddf1332b11b39fa6b7952c3d7b4a438
ruizhen88 <yejin@workmate.asia>
2021-09-15 19:50:52 +0900

    add: mobile view for banner icon

--- src/components/ArrowCircle.js
+++ src/components/ArrowCircle.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { useMediaQuery, useTheme } from "@material-ui/core";
 
 const ArrowCircle = ({ style }) => {
+  const theme = useTheme();
+  const isSm = useMediaQuery(theme.breakpoints.down("sm"), {
+    defaultMatches: true,
+  });
   return (
     <div
       style={{
-        width: "20px",
-        height: "20px",
+        width: isSm ? "14px" : "20px",
+        height: isSm ? "14px" : "20px",
         backgroundColor: "white",
         borderRadius: "100%",
         position: "relative",
