af6bd03b677f61287aee8d1b1612a3d30cca8eae
ruizhen88 <yejin@workmate.asia>
2022-02-03 21:41:26 +0900

    add: custom theme

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,48 @@
 import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
-// A custom theme for this app
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFFFFF",
+      main: "#000000",
     },
     secondary: {
-      main: "#19857b",
+      main: "#1302F4",
     },
     error: {
       main: red.A400,
     },
+    background: {
+      default: "#FFFFFF",
+    },
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          padding: "10px 20px",
+          borderRadius: 0,
+        },
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: `
+        body {
+          font-size: 16px;
+        }
+      `,
+    },
+  },
+  // "& a": {
+  //   textDecoration: "none",
+  //   color: "initial",
+  // },
+
+  // MuiChip: {
+  //   root: {
+  //     padding: "0 12px",
+  //   },
+  // },
 });
 
 export default theme;
